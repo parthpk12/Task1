2ad46444081a669c6c90da3c84c4a7f5d2610fd4
Parth <parthpersonal12@gmail.com>
2025-07-23 13:17:42 +0530

    stock and admin api routes create

---
+++ routes/adminRoute.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const checkAuth = require("../middlewares/tokenmiddleware");
+const checkRole = require("../middlewares/checkRolemiddleware");
+const {
+  getAllUsers,
+  deleteUser,
+  addRole,
+  deleteRole,
+} = require("../controllers/adminController");
+const adminRouter = express.Router();
+
+adminRouter.get("/getAllUsers", checkAuth, checkRole(["Admin"]), getAllUsers);
+
+adminRouter.delete(
+  "/deleteUser/:id",
+  checkAuth,
+  checkRole(["Admin"]),
+  deleteUser
+);
+
+adminRouter.post("/addRole", checkAuth, checkRole(["Admin"]), addRole);
+
+adminRouter.delete(
+  "/deleteRole/:id",
+  checkAuth,
+  checkRole(["Admin"]),
+  deleteRole
+);
+
+module.exports = adminRouter;
--- routes/stockRoute.js
+++ routes/stockRoute.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const checkAuth = require("../middlewares/tokenmiddleware");
 const checkRole = require("../middlewares/checkRolemiddleware");
+const { createStock, updateStock, deleteStock, getAllStocks } = require("../controllers/stockController");
 const stockRouter = express.Router();
 
-stockRouter.post("/create",checkAuth,checkRole["Order manager"],createStock);
+stockRouter.post("/create",checkAuth,checkRole(["Order manager"]),createStock);
+
+stockRouter.put("/update",checkAuth,checkRole(["Order manager"]),updateStock);
+
+stockRouter.delete("/delete/:id",checkAuth , checkRole(["Order manager"]) , deleteStock);
+
+stockRouter.get("/getAllStock",checkAuth,checkRole(["Order manager"]),getAllStocks);
 
 module.exports = stockRouter;
