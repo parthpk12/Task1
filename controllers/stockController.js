d942267ffa05d3d9df069d6a8212b18a0b19727d
Parth <parthpersonal12@gmail.com>
2025-07-23 11:24:04 +0530

    working on stock

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,6 +1,12 @@
 const createStock = async (req, res) => {
   try {
     const { id, quantity } = req.body;
+
+    if(!id || !quantity){
+     return res.send("Filed missing");
+    }
+
+    
   } catch (err) {
     res.send("Error for creating stocks" + err.message);
   }
