a24b9568174dbd6cc8911a13c2219234a4c375ea
Parth <parthpersonal12@gmail.com>
2025-07-23 16:32:07 +0530

    postgres add

--- utils/db.js
+++ utils/db.js
@@ -1,19 +1,58 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
+let sequelize;
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    logging: false,
+  });
+  console.log("Using PostgreSQL database configuration.");
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: "mysql",
+      logging: false,
+    }
+  );
+  console.log("Using MySQL database configuration (local).");
+}
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+module.exports = { connectDB, sequelize };
 
+// const mysql = require("mysql2/promise");
+// require("dotenv").config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+// const db = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
 
-db.getConnection()
-  .then(() => console.log("DB connected"))
-  .catch((err) => console.error("DB connection error:", err.message));
+// db.getConnection()
+//   .then(() => console.log("DB connected"))
+//   .catch((err) => console.error("DB connection error:", err.message));
 
-module.exports = db;
+// module.exports = db;
 
 //  async function main(){
 //   const db = await mysql.createConnection({
