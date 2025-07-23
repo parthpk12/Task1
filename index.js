bc30f5e74a7ceac94cb3404388ea91dde09a125e
Parth <parthpersonal12@gmail.com>
2025-07-23 13:20:31 +0530

    admin route created

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const userRouter = require("./routes/userRoute");
 const productRouter = require("./routes/productRoute");
-const stockRouter = require("./routes/stockRoute")
+const stockRouter = require("./routes/stockRoute");
+const adminRouter = require("./routes/adminRoute");
 const port = 3000;
 
 app.use(express.json());
@@ -13,8 +14,7 @@ app.use(cookieParser());
 app.use("/user", userRouter);
 app.use("/product", productRouter);
 app.use("/stock", stockRouter);
-
-// app.use("/adminCruds" , adminCruds);
+app.use("/admin" , adminRouter);
 
 app.listen(port, () => {
   console.log(`server is listening at PORT ${port}`);
