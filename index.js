6387c0cdbd90f26b606f121721c4f3f94018af64
Parth <parthpersonal12@gmail.com>
2025-07-24 17:18:59 +0530

    cors issue resolve

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ const userRouter = require("./routes/userRoute");
 const productRouter = require("./routes/productRoute");
 const stockRouter = require("./routes/stockRoute");
 const adminRouter = require("./routes/adminRoute");
+const cors = require("cors");
 require("dotenv").config();
 
-
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/user", userRouter);
 app.use("/product", productRouter);
 app.use("/stock", stockRouter);
 app.use("/admin", adminRouter);
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening at PORT ${process.env.PORT}`);
