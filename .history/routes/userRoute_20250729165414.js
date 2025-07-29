4c2d0f549a81e82ecfffd465d37b6d5398ad887c
Parth <parthpersonal12@gmail.com>
2025-07-29 16:58:27 +0530

    added product_feedback feature

---
+++ .history/routes/userRoute_20250729165414.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const checkAuth = require("../middlewares/tokenmiddleware");
+const userRouter = express.Router();
+const {
+  userRegister,
+  userLogin,
+  userLogout,
+  ProductFeedbackGive
+} = require("../controllers/userController");
+
+userRouter.post("/register", userRegister);
+
+userRouter.post("/login",userLogin);
+
+userRouter.post("/logout", checkAuth , userLogout);
+
+userRouter.post("/product_feedback/:pid",checkAuth,ProductFeedbackGive);
+
+
+module.exports = userRouter;
